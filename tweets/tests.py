606d9811544f97e66d79dc4789175b7b919b1bbd
Irina Serova <isserova@gmail.com>
2018-07-16 11:04:42 -0700

    new styles added

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from .models import Tweet
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+
+class TweetModelTestCase(TestCase):
+
+
+
+
+    def setUp(self):
+        test_user = User.objects.create(username='testName')
+
+    def test_tweet_item(self):
+        obj = Tweet.objects.create(
+                user= User.objects.first(),
+                content='Test content'
+            )
+        self.assertTrue(obj.content == "Test content")
+        self.assertTrue(obj.id == 1)
+        #self.assertEqual(obj.id, 1)
+        absolute_url = reverse("tweet:detail", kwargs={"pk": 1})
+        self.assertEqual(obj.get_absolute_url(), absolute_url)
+
+    def test_tweet_url(self):
+        obj = Tweet.objects.create(
+                user= User.objects.first(),
+                content='Test content'
+            )
+        absolute_url = reverse("tweet:detail", kwargs={"pk": obj.pk})
+        self.assertEqual(obj.get_absolute_url(), absolute_url)
+
