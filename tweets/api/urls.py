5c146fce50d9b4639f3b144ce443170182036266
Irina Serova <isserova@gmail.com>
2018-07-16 13:08:44 -0700

    added new styles

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -4,6 +4,7 @@ from django.views.generic.base import RedirectView
 
 from .views import (
     TweetListAPIView,
+    
     # LikeToggleAPIView,
     # RetweetAPIView,
     # TweetCreateAPIView,
@@ -14,8 +15,8 @@ from .views import (
 
 urlpatterns = [
     url(r'^$', TweetListAPIView.as_view(), name='list'), 
-    url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # /tweet/create/
-    url(r'^(?P<pk>\d+)/$', TweetDetailAPIView.as_view(), name='detail'),
-    url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name='like-toggle'),
-    url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'), #/api/tweet/id/tweet/
+    # url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # /tweet/create/
+    # url(r'^(?P<pk>\d+)/$', TweetDetailAPIView.as_view(), name='detail'),
+    # url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name='like-toggle'),
+    # url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'), #/api/tweet/id/tweet/
 ]
--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,32 +1,10 @@
-from tweets.model import Tweet
+from tweets.models import Tweet
+
 from .serializers import ModelTweetSerializer
 from rest_framework import generics
 
 class TweetListAPIView(generics.ListAPIView):
-    serializer_class = TweetModelSerializer
-    pagination_class = StandardResultsPagination
+    serializer_class = ModelTweetSerializer
+    queryset = Tweet.objects.all()
 
-    def get_serializer_context(self, *args, **kwargs):
-        context = super(TweetListAPIView, self).get_serializer_context(*args, **kwargs)
-        context['request'] = self.request
-        return context
-
-    def get_queryset(self, *args, **kwargs):
-        requested_user = self.kwargs.get("username")
-        
-        if requested_user:
-            qs = Tweet.objects.filter(user__username=requested_user).order_by("-timestamp")
-        else:
-            im_following = self.request.user.profile.get_following() # none
-            qs1 = Tweet.objects.filter(user__in=im_following)
-            qs2 = Tweet.objects.filter(user=self.request.user)
-            qs = (qs1 | qs2).distinct().order_by("-timestamp")
-        
-        query = self.request.GET.get("q", None)
-        if query is not None:
-            qs = qs.filter(
-                    Q(content__icontains=query) |
-                    Q(user__username__icontains=query)
-                    )
-        return qs
-
+   
