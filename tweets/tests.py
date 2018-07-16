3d56715a875e1d469a3ee93d74bfbfa7dc6eb8fb
Irina Serova <isserova@gmail.com>
2018-07-16 11:10:15 -0700

    new styles added

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import Tweet
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 
 User = get_user_model()
@@ -9,20 +10,16 @@ User = get_user_model()
 
 class TweetModelTestCase(TestCase):
 
-
-
-
     def setUp(self):
-        test_user = User.objects.create(username='testName')
+        test_user = User.objects.create(username='Tester')
 
-    def test_tweet_item(self):
+    def test_tweet(self):
         obj = Tweet.objects.create(
                 user= User.objects.first(),
                 content='Test content'
             )
         self.assertTrue(obj.content == "Test content")
         self.assertTrue(obj.id == 1)
-        #self.assertEqual(obj.id, 1)
         absolute_url = reverse("tweet:detail", kwargs={"pk": 1})
         self.assertEqual(obj.get_absolute_url(), absolute_url)
 
