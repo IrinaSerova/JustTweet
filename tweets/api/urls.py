82f77331f722e85db7689208e1f1a639693b142e
Irina Serova <isserova@gmail.com>
2018-07-17 12:37:17 -0700

    new styles added

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -10,8 +10,5 @@ from .views import (
 
 urlpatterns = [
     url(r'^$', TweetListAPIView.as_view(), name='list'), 
-    # url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # /tweet/create/
-    # url(r'^(?P<pk>\d+)/$', TweetDetailAPIView.as_view(), name='detail'),
-    # url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name='like-toggle'),
-    # url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'), #/api/tweet/id/tweet/
+
 ]
