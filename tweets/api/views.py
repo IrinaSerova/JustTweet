866cd09f913ca623aa9585a53ad499c9c9497860
Irina Serova <isserova@gmail.com>
2018-07-18 12:02:18 -0700

    new styles added

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,10 +1,36 @@
 from tweets.models import Tweet
+from django.db.models import Q
 
 from .serializers import ModelTweetSerializer
 from rest_framework import generics
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = ModelTweetSerializer
-    queryset = Tweet.objects.all()
+    
+
+    def get_queryset(self, *args, **kwargs):
+    	qs = Tweet.objects.all()
+    	print(self.request.GET)
+    	query = self.request.GET
+
+    	# requested_user = self.kwargs.get("username")
+
+    	# if requested_user:
+    	# 	qs = Tweet.objects.filter(user__username=requested_user).order_by("-timestamp")
+    	# else:
+     #        im_following = self.request.user.profile.get_following() # none
+     #        qs1 = Tweet.objects.filter(user__in=im_following)
+     #        qs2 = Tweet.objects.filter(user=self.request.user)
+     #        qs = (qs1 | qs2).distinct().order_by("-timestamp")
+
+     #        query = self.request.GET.get("q", None)
+     #        if query is not None:
+     #        	qs = qs.filter(
+     #        		Q(content__icontains=query) |
+     #        		Q(user__username__icontains=query)
+     #        		)
+     #        	return qs
+
+
 
    
