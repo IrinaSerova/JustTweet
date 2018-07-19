af49b77cabcc2745e89387d2ce7832be84bc8f5f
Irina Serova <isserova@gmail.com>
2018-07-19 00:01:04 -0700

    new styles added

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -2,16 +2,21 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
-	TweetListView, 
-	TweetDetailView, 
-	TweetCreateView, 
-	TweetUpdateView, 
-	TweetDeleteView)
+	TweetListAPIView,
+	TweetCreateAPIView
+	# TweetListView, 
+	# TweetDetailView, 
+	# TweetCreateView, 
+	# TweetUpdateView, 
+	# TweetDeleteView
+	)
 
 urlpatterns = [
-    url(r'^$', TweetListView.as_view(), name="list"), # localhost:4000/tweet/
-    url(r'^create/$', TweetCreateView.as_view(), name='create'),
-    url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
-    url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'),
-    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), # localhost:4000/tweet/id_of_tweet
+	url(r'^$', TweetListAPIView.as_view(), name='list'),
+	url(r'^create/$', TweetCreateAPIView.as_view(), name='create'),
+    # url(r'^$', TweetListView.as_view(), name="list"), # localhost:4000/tweet/
+    # url(r'^create/$', TweetCreateView.as_view(), name='create'),
+    # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
+    # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'),
+    # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), # localhost:4000/tweet/id_of_tweet
 ]
--- tweets/urls.py
+++ tweets/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     url(r'^create/$', TweetCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
     url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'),
-    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),
-   
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), # localhost:4000/tweet/id_of_tweet
 ]
