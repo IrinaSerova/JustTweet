fd608bf4e784ab049a82ca2120ea74615794fe22
Irina Serova <isserova@gmail.com>
2018-07-29 15:04:15 -0700

    register and login added

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.views.generic.base import RedirectView
+
+from .views import (
+	UserDetailView
+	)
+
+urlpatterns = [
+    url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'),
+    ]
