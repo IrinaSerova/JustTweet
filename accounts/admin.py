cad74a860f1a10a8ccf5340d7a8231c9bce4e4e1
Irina Serova <isserova@gmail.com>
2018-07-29 15:01:25 -0700

    register and login added

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+
 # Register your models here.
+
+from .models import UserProfile
+
+
+admin.site.register(UserProfile)
